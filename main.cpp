6892ff4c4934b30a350a1df001ef0e0cca9904ec
mdawso <dawsonmathew08@gmail.com>
2024-12-10 02:05:45 +0000

    much more stable collisions

--- main.cpp
+++ main.cpp
@@ -5,34 +5,45 @@
 #define WINDOW_TITLE "bouncy"
 #define gravity 10
 
+struct Vector2i
+{
+    int x;
+    int y;
+};
+
 class BouncySquare
 {
     public:
 
-    float pos_x, pos_y;
-    float size_x, size_y;
+    Vector2 pos;
+    Vector2i size;
     Color colour;
     Vector2 velocity;
+    
+    Vector2 prev_pos;
 
-    BouncySquare(float pos_x, float pos_y, float size_x, float size_y, Color colour, Vector2 velocity) : pos_x(pos_x), pos_y(pos_y), size_x(size_x), size_y(size_y), colour(colour), velocity(velocity) {}
+    BouncySquare(Vector2 pos, Vector2i size, Color colour, Vector2 velocity) : pos(pos), size(size), colour(colour), velocity(velocity) {}
 
     int update(float dt) {
         // apply linear velocities
-        pos_x += velocity.x * dt;
-        pos_y += velocity.y * dt;
+        pos.x += velocity.x * dt;
+        pos.y += velocity.y * dt;
         // apply gravity
         velocity.y += gravity;
         // collide with walls
-        if(pos_x < 0 || pos_x + size_x > WINDOW_X)
+        if(pos.x < 0 || pos.x + size.x > WINDOW_X)
         {
+            pos = prev_pos;
             velocity.x = -velocity.x;
         }
-        if(pos_y < 0 || pos_y + size_y > WINDOW_Y)
+        if(pos.y < 0 || pos.y + size.y > WINDOW_Y)
         {
+            pos = prev_pos;
             velocity.y = -velocity.y;
         }
+        prev_pos = pos;
         // draw the rect
-        DrawRectangle(pos_x, pos_y, size_x, size_y, colour);
+        DrawRectangle(pos.x, pos.y, size.x, size.y, colour);
         return 0;
     }
 };
@@ -45,14 +56,13 @@ int main()
     InitWindow(WINDOW_X, WINDOW_Y, WINDOW_TITLE);
     SetTargetFPS(60);
 
-    for(int i = 0; i < num_squares; i++) {
-        float w = GetRandomValue(25,75);
-        float h = GetRandomValue(25,75);
-        float x = GetRandomValue(w, WINDOW_X - w); 
-        float y = GetRandomValue(h, WINDOW_Y - h);
+    for(int i = 0; i < num_squares; i++) 
+    {
+        Vector2i size = {GetRandomValue(25,75), GetRandomValue(25,75)};
+        Vector2 pos = {(float)GetRandomValue(0, WINDOW_X - (int)size.x), (float)GetRandomValue(0, WINDOW_Y - (int)size.y)};
         Color colour = {(unsigned char)GetRandomValue(0,255), (unsigned char)GetRandomValue(0,255), (unsigned char)GetRandomValue(0,255), 0xff};
         Vector2 velocity = {(float)GetRandomValue(-250,250), (float)GetRandomValue(-250,250)};
-        squares[i] = new BouncySquare(x, y, w, h, colour, velocity);
+        squares[i] = new BouncySquare(pos, size, colour, velocity);
     }
 
     while(!WindowShouldClose())
